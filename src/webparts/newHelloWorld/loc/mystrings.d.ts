6a5513399c15bb77d0402b3d8aaf7862143a1691
Reynaldo Souza Santos <reynaldo.ss@gmail.com>
2025-12-15 13:25:02 -0300

    WebPart NewHelloWorld criada

---
+++ src/webparts/newHelloWorld/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface INewHelloWorldWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'NewHelloWorldWebPartStrings' {
+  const strings: INewHelloWorldWebPartStrings;
+  export = strings;
+}
