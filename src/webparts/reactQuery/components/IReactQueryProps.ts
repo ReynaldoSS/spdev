a2b162fbfb06c873ca92e41af5baea64091d7358
Reynaldo Souza Santos <reynaldo.ss@gmail.com>
2025-12-16 09:42:39 -0300

    ReactQuery inicializada

---
+++ src/webparts/reactQuery/components/IReactQueryProps.ts
@@ -0,0 +1,7 @@
+export interface IReactQueryProps {
+  description: string;
+  isDarkTheme: boolean;
+  environmentMessage: string;
+  hasTeamsContext: boolean;
+  userDisplayName: string;
+}
---
+++ src/webparts/reactQuery/loc/mystrings.d.ts
@@ -0,0 +1,19 @@
+declare interface IReactQueryWebPartStrings {
+  PropertyPaneDescription: string;
+  BasicGroupName: string;
+  DescriptionFieldLabel: string;
+  AppLocalEnvironmentSharePoint: string;
+  AppLocalEnvironmentTeams: string;
+  AppLocalEnvironmentOffice: string;
+  AppLocalEnvironmentOutlook: string;
+  AppSharePointEnvironment: string;
+  AppTeamsTabEnvironment: string;
+  AppOfficeEnvironment: string;
+  AppOutlookEnvironment: string;
+  UnknownEnvironment: string;
+}
+
+declare module 'ReactQueryWebPartStrings' {
+  const strings: IReactQueryWebPartStrings;
+  export = strings;
+}
